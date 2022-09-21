960694d8107859e89c1c59846b69858dfb415a30
mmerloy <a.mmerloy@gmail.com>
2022-09-21 19:52:55 +0300

    Добавлено меню для возможности выбора декодера и кодера

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,18 +1,53 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 
 namespace Lab1
 {
     class Program
     {
+        static int ChooseComand()
+        {
+            Console.WriteLine("Choose operation:\n");
+            Console.WriteLine("0-exit from program\n1-encode file\n2-decode file\n");
+            Console.WriteLine("---------------------------------------------------");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static (string pathRead, string pathWrite) GetFileNames()
+        {
+            Console.WriteLine("Enter the file name to read: ");
+            string pathRead = Console.ReadLine();
+            Console.WriteLine("Enter the file name to write: ");
+            string pathWrite = Console.ReadLine();
+
+            return (pathRead, pathWrite);
+        }
+
         static void Main(string[] args)
         {
-            //Encoder encoder = new Encoder();
-            //encoder.Encode("test.txt", "test2.txt");
+            while (true)
+            {
+                switch (ChooseComand())
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        Console.WriteLine("Encoding...");
+                        var (pathRead, pathWrite) = GetFileNames();
+                        Encoder encoder = new Encoder();
+                        encoder.Encode(pathRead, pathWrite);
+                        break;
+                    case 2:
+                        Console.WriteLine("Decoding...");
+                        (pathRead, pathWrite) = GetFileNames();
+                        Decoder decoder = new Decoder();
+                        decoder.Decode(pathRead, pathWrite);
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-            Decoder decoder = new Decoder();
-            decoder.Decode("test2.txt", "test.txt");
         }
     }
 }
