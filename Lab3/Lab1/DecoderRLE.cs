a42a723a43164146abfa41b1567850656de13865
pavel557 <79131686+pavel557@users.noreply.github.com>
2022-10-31 19:25:19 +0300

    Add files via upload

---
+++ Lab3/Lab1/DecoderRLE.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    class DecoderRLE
+    {
+        public void Decode(string pathRead, string folderPathWrite)
+        {
+            byte[] buffer;
+            Header newHeader = new Header();
+            using (FileStream fstream = File.OpenRead(pathRead))
+            {
+                // выделяем массив для считывания данных из файла
+                buffer = new byte[fstream.Length];
+                // считываем данные
+
+                fstream.Read(buffer);
+            }
+
+            //проверка сигнатуры
+            int i = 0;
+            int edge = newHeader.Signature.Length;
+            byte[] signature = new byte[edge];
+            for (int j = 0; i < edge && i < buffer.Length; i++, j++)
+            {
+                signature[j] = buffer[i];
+                if (signature[j] != newHeader.Signature[j])
+                {
+                    Console.WriteLine("Неизвестная сигнатура");
+                    return;
+                }
+            }
+
+            byte[] formatVersion = new byte[4];
+            edge += BitConverter.GetBytes(newHeader.FormatVersion).Length;
+            for (int j = 0; i < edge && i < buffer.Length; i++, j++)
+            {
+                formatVersion[j] = buffer[i];
+                if (formatVersion[j] != BitConverter.GetBytes(newHeader.FormatVersion)[j])
+                {
+                    Console.WriteLine("Неактуальная версия");
+                    return;
+                }
+            }
+
+            byte[] сompressionAndProtectionAlgorithmCode = new byte[1];
+            edge += newHeader.CompressionAndProtectionAlgorithmCode.Length;
+            for (int j = 0; i < edge && i < buffer.Length; i++, j++)
+            {
+                сompressionAndProtectionAlgorithmCode[j] = buffer[i];
+                if (сompressionAndProtectionAlgorithmCode[j] != newHeader.CompressionAndProtectionAlgorithmCode[j])
+                {
+                    Console.WriteLine("Код алгоритма не совпадает");
+                    return;
+                }
+            }
+
+            byte[] fileLength = new byte[4];
+            edge += BitConverter.GetBytes(newHeader.FileLength).Length;
+            for (int j = 0; i < edge && i < buffer.Length; i++, j++)
+            {
+                fileLength[j] = buffer[i];
+            }
+
+            //int fileLengthInt = BitConverter.ToInt32(fileLength, 0);
+            //if (fileLengthInt != buffer.Length - edge)
+            //{
+            //    Console.WriteLine("Размер файла не совпадает");
+            //    return;
+            //}
+
+
+            while (i < buffer.Length)
+            {
+                //ex: 7asd.txt10aaaaaaaaaa
+
+                byte[] nameLenInBytes = new byte[4];
+                for (int b = 0; b < 4; b++, i++)
+                    nameLenInBytes[b] = buffer[i];
+                int nameLen = BitConverter.ToInt32(nameLenInBytes);//7
+
+                byte[] nameInBytes = new byte[nameLen];
+                for (int b = 0; b < nameLen; b++, i++)
+                    nameInBytes[b] = buffer[i];
+                string fName = Encoding.UTF8.GetString(nameInBytes);//asd.txt
+
+                byte[] fileBufferBytesCount = new byte[4];
+                for (int b = 0; b < 4; b++, i++)
+                    fileBufferBytesCount[b] = buffer[i];
+                int fileBufferLen = BitConverter.ToInt32(fileBufferBytesCount);//10
+
+                byte[] fileBuffer = new byte[fileBufferLen];
+                for (int b = 0; b < fileBufferLen; b++, i++)
+                    fileBuffer[b] = buffer[i];//aaaaaaaaaa
+
+                List<byte> newFile = new List<byte>();
+
+                for (int b = 0; b < fileBuffer.Length; b++)
+                {
+                    byte bt = fileBuffer[b];
+                    string s = Convert.ToString(bt, 2);
+                    while (s.Length < 8)
+                    {
+                        s = "0" + s;
+                    }
+                    if (s[0] == '1')
+                    {
+                        char[] charMas = new char[7];
+                        s.CopyTo(1, charMas, 0, 7);
+                        s = "0" + new string(charMas);
+                        int count = Convert.ToByte(s, 2);
+                        b++;
+                        
+                        while (count > 0)
+                        {
+                            byte cr = fileBuffer[b];
+                            newFile.Add(cr);
+                            count--;
+                            b++;
+                        }
+                    }
+                    else
+                    {
+                        int count = Convert.ToByte(s, 2);
+                        b++;
+                        while (count > 0)
+                        {
+                            byte cr = fileBuffer[b];
+                            newFile.Add(cr);
+                            count--;
+                        }
+                    }
+                }
+
+
+                //"C:\Users\123m\source\repos\OTIK_Lab1\Lab1\Lab1\bin\Debug\netcoreapp3.1\files\
+                //MinecraftEdu1.pdf
+                Directory.CreateDirectory(folderPathWrite);
+                string fullFilePath = folderPathWrite + "\\" + fName;
+                //C:\Users\123m\source\repos\OTIK_Lab1\Lab1\Lab1\bin\Debug\netcoreapp3.1\files\MinecraftEdu1.pdf
+
+                File.WriteAllBytes(fullFilePath, newFile.ToArray());
+            }
+
+            Console.WriteLine("Раскодировано");
+        }
+    }
+}
---
+++ Lab3/Lab1/EncoderRLE.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class EncoderRLE
+    {
+        public void Encode(string folderPathRead, string pathWrite)
+        {
+            //Берем все файлы из папки
+            List<string> files = Directory.EnumerateFiles(folderPathRead).ToList();
+            //Создаем для каждого файла свой буфер
+            List<byte[]> filesBuffers = new List<byte[]>(files.Count);
+            //Заплнаяем буферы
+            for (int i = 0; i < files.Count; i++)
+            {
+                using (FileStream fstream = File.OpenRead(files[i]))
+                {
+                    // выделяем массив для считывания данных из файла
+                    filesBuffers.Add(new byte[fstream.Length]);
+                    // считываем данные
+                    fstream.Read(filesBuffers[i]);
+                }
+            }
+
+            List<List<byte>> newFilesBytes = new List<List<byte>>();
+
+            foreach (byte[] fileBytes in filesBuffers)
+            {
+                List<byte> newFileBytes = new List<byte>();
+                List<byte> buff = new List<byte>();
+                bool isRepeat = true;
+                for (int i=0; i<fileBytes.Length; i++)
+                {
+                    if (buff.Count == 0)
+                    {
+                        buff.Add(fileBytes[i]);
+                    }
+                    else if (fileBytes[i] == buff[buff.Count - 1])
+                    {
+                        if (isRepeat)
+                        {
+                            buff.Add(fileBytes[i]);
+                        }
+                        else
+                        {
+                            isRepeat = true;
+                            int countChar = buff.Count;
+                            byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                            int newCountChar = (int)countCharInByte[0] + 128;
+                            byte[] newcountCharInByte = BitConverter.GetBytes(newCountChar);
+                            byte finalValue = newcountCharInByte[0];
+                            newFileBytes.Add(finalValue);
+                            foreach (byte b in buff)
+                            {
+                                newFileBytes.Add(b);
+                            }
+                            buff.Clear();
+                            buff.Add(fileBytes[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (isRepeat && buff.Count >= 3)
+                        {
+                            int countChar = buff.Count;
+                            byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                            byte finalValue = countCharInByte[0];
+                            newFileBytes.Add(finalValue);
+                            newFileBytes.Add(buff[0]);
+                            buff.Clear();
+                            buff.Add(fileBytes[i]);
+
+                        }
+                        else
+                        {
+                            buff.Add(fileBytes[i]);
+                            isRepeat = false;
+                        }
+                    }
+
+                    if (buff.Count == 127)
+                    {
+                        if (isRepeat)
+                        {
+                            int countChar = buff.Count;
+                            byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                            byte finalValue = countCharInByte[0];
+                            newFileBytes.Add(finalValue);
+                            newFileBytes.Add(buff[0]);
+                            buff.Clear();
+
+                        }
+                        else
+                        {
+                            int countChar = buff.Count;
+                            byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                            int newCountChar = (int)countCharInByte[0] + 128;
+                            byte[] newcountCharInByte = BitConverter.GetBytes(newCountChar);
+                            byte finalValue = newcountCharInByte[0];
+                            newFileBytes.Add(finalValue);
+                            foreach (byte b in buff)
+                            {
+                                newFileBytes.Add(b);
+                            }
+                            buff.Clear();
+                        }
+                    }
+                }
+
+                if (buff.Count != 0)
+                {
+                    if (isRepeat)
+                    {
+                        int countChar = buff.Count;
+                        byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                        byte finalValue = countCharInByte[0];
+                        newFileBytes.Add(finalValue);
+                        newFileBytes.Add(buff[0]);
+                        buff.Clear();
+
+                    }
+                    else
+                    {
+                        int countChar = buff.Count;
+                        byte[] countCharInByte = BitConverter.GetBytes(countChar);
+                        int newCountChar = (int)countCharInByte[0] + 128;
+                        byte[] newcountCharInByte = BitConverter.GetBytes(newCountChar);
+                        byte finalValue = newcountCharInByte[0];
+                        newFileBytes.Add(finalValue);
+                        foreach (byte b in buff)
+                        {
+                            newFileBytes.Add(b);
+                        }
+                        buff.Clear();
+                    }
+                }
+
+                newFilesBytes.Add(newFileBytes);
+            }
+
+            //Записываем буферы файлов поочередно в архив
+            using (FileStream fstream = new FileStream(pathWrite, FileMode.OpenOrCreate))
+            {
+                int totalLength = newFilesBytes.Sum(b => b.Count);
+
+                Header header = new Header(totalLength);
+                fstream.Write(header.Signature);
+                fstream.Write(BitConverter.GetBytes(header.FormatVersion));
+                fstream.Write(header.CompressionAndProtectionAlgorithmCode);
+                fstream.Write(BitConverter.GetBytes(header.FileLength));
+
+                for (int i = 0; i < newFilesBytes.Count; i++)
+                {//! 
+                    string currentFileName = new FileInfo(files[i]).Name;
+                    //Получили полный путь файла в байтах 
+                    byte[] fileNameInbytes = Encoding.UTF8.GetBytes(currentFileName);
+                    //Записываем длину названия файла в байтах
+                    fstream.Write(BitConverter.GetBytes(fileNameInbytes.Length));
+                    //Записываем это назване перед файлом
+                    fstream.Write(fileNameInbytes);
+
+                    byte[] buffer = newFilesBytes[i].ToArray();
+                    //Записали длину файла
+                    fstream.Write(BitConverter.GetBytes(buffer.Length));
+                    //Записываем буфер файла
+                    fstream.Write(buffer);
+                }
+
+                Console.WriteLine("Закодировано");
+            }
+        }
+    }
+}
